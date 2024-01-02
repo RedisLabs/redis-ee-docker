9769f255942c929152dc93049c5a279e13742bf7
vladvildanov <divinez122@outlook.com>
2024-01-02 15:49:14 +0200

    Fixed ENV variable naming typo

--- create_db.sh
+++ create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -u $RE_USERNAME:$RE_PASS -H "Content-type: application/json" \
-             -d '{"name": "db", "port": '$RE_DB_PORT', "memory_size": 1273741824, "replication": false, "eviction_policy": "volatile-lru", "sharding": true, "shards_count": 3, "module_list": [{"module_args": "", "module_name": "ReJSON"},{"module_args": "", "module_name": "search"},{"module_args": "", "module_name": "timeseries"},{"module_args": "", "module_name": "bf"}], "oss_cluster": '$RE_USE_RE_USE_OSS_CLUSTER', "proxy_policy": "all-nodes", "shards_placement": "sparse", "shard_key_regex": [{"regex": ".*\\{(?<tag>.*)\\}.*"},{"regex": "(?<tag>.*)"}]}' \
+             -d '{"name": "db", "port": '$RE_DB_PORT', "memory_size": 1273741824, "replication": false, "eviction_policy": "volatile-lru", "sharding": true, "shards_count": 3, "module_list": [{"module_args": "", "module_name": "ReJSON"},{"module_args": "", "module_name": "search"},{"module_args": "", "module_name": "timeseries"},{"module_args": "", "module_name": "bf"}], "oss_cluster": '$RE_USE_OSS_CLUSTER', "proxy_policy": "all-nodes", "shards_placement": "sparse", "shard_key_regex": [{"regex": ".*\\{(?<tag>.*)\\}.*"},{"regex": "(?<tag>.*)"}]}' \
              -k -X POST https://localhost:9443/v1/bdbs
